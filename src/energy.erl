8f25203d55ff67942fee4503dce8bf33bd2b8ad1
tjm1518 <54404449+tjm1518@users.noreply.github.com>
2021-05-15 12:21:26 +0100

    removed HRL deps

--- src/energy.erl
+++ src/energy.erl
@@ -1,10 +1,8 @@
 -module(energy).
 -export([init/1]).
 
--include("pins.hrl").
-
 % Energy gets analogue & input only pins
--define(ENERGY_PINS, [?A0,?A1,?A2,?A3,?A4,?A5]).
+-define(ENERGY_PINS, []).
 
 init(GPIO) ->
   erlang:display("Hello World from Energy").
